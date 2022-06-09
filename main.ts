7fd1f271df1cc5e68e7d0568f89be3961f69c6d9
qinyuehaothomas <74227996+qinyuehaothomas@users.noreply.github.com>
2022-06-09 12:17:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
-
+// TODO: Initalise WIFI
+function InitWIFI (SSID: string, Pswd: string) {
+    esp8266.init(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+    esp8266.connectWiFi(SSID, Pswd)
+    if (esp8266.isWifiConnected()) {
+        basic.showLeds(`
+            # # # . .
+            . . . # .
+            # # . . #
+            . . # . #
+            # . # . #
+            `)
+        return true
+    } else {
+        basic.showIcon(IconNames.No)
+        return false
+    }
+}
 function pHConversion (AnalogReading: number) {
     return Math.round(14 - (AnalogReading * (5 / 1023) * -4.8 + 17))
 }
@@ -8,23 +25,6 @@ function WaterTempConverter (AnalogReading: number) {
     // Temperature= 1177692.5 / (3950 + 298.15 * ln(V/(3.3-V)) - 273.5.
     return 1177692.5 / (3950 + 298.15 * (Math.log(V/(3-V)) / Math.log(Math.E))) - 223.15
 }
-// TODO: Initalise WIFI
-function InitWIFI (SSID: string, Pswd: string) {
-    esp8266.isESP8266Initialized()
-    esp8266.init(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-    esp8266.connectWiFi(SSID,Pswd)
-    if (esp8266.isWifiConnected()) {
-        basic.showLeds(`
-            # # # . .
-            . . . # .
-            # # . . #
-            . . # . #
-            # . # . #
-            `)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-}
 // TODO: Just For Tuning
 function SerialTuning () {
     serial.writeValue("pH", pH)
@@ -32,11 +32,34 @@ function SerialTuning () {
     serial.writeValue("Light", Light)
     serial.writeValue("Temp(℃)", SurroundingTemperature)
 }
+// TODO: Send All Shit
+function FormattedRequest () {
+    esp8266.uploadThingspeak(
+    "KTANII17SM9PCOI2",
+    pH,
+    WaterTemperature,
+    Light,
+    SurroundingTemperature,
+    BoatID
+    )
+    if (!(esp8266.isThingspeakUploaded())) {
+        basic.showIcon(IconNames.Confused)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            . . . # .
+            # # . . #
+            . . # . #
+            # . # . #
+            `)
+    }
+}
 let WaterTemperature = 0
 let pH = 0
-let SurroundingTemperature = 0
-let V = 0
+let BoatID = 0
 let Light = 0
+let V = 0
+let SurroundingTemperature = 0
 function ReadLightIntensity(lightintensitypin: AnalogPin): number {
     let voltage = 0;
     let lightintensity = 0;
@@ -75,35 +98,18 @@ function ReadTemperature(CorF: string, temppin: AnalogPin): number {
             return 0
     }
 }
-let BoatID = 925
-InitWIFI("Redmi 9T", "87654321")
-
-// TODO: Send All Shit
-function FormattedRequest() {
-    esp8266.uploadThingspeak("KTANII17SM9PCOI2", pH,WaterTemperature,Light,SurroundingTemperature,BoatID)
-    if (!esp8266.isThingspeakUploaded()){
-        basic.showIcon(IconNames.No)
-    } else {
-        basic.showLeds(`
-            # # # . .
-            . . . # .
-            # # . . #
-            . . # . #
-            # . # . #
-            `)
-
-    }
-    
-}
+BoatID = 925
+let WifiConnected=false
+// SerialTuning()
 basic.forever(function () {
     // pH conversion Table:
     // https://raw.githubusercontent.com/DFRobot/DFRobotMediaWikiImage/master/Image/Ph-mv.jpg
     pH = pHConversion(pins.analogReadPin(AnalogPin.P1))
     SurroundingTemperature = ReadTemperature("C", AnalogPin.P2)
-    // max: tab water, no visble obstruction
-    // min: milo
-    Light = ReadLightIntensity(AnalogPin.P3)
-    WaterTemperature = WaterTempConverter(pins.analogReadPin(AnalogPin.P4))
+Light = ReadLightIntensity(AnalogPin.P3)
+WaterTemperature = WaterTempConverter(pins.analogReadPin(AnalogPin.P4))
     FormattedRequest()
-    // SerialTuning()
+    if (!WifiConnected){
+        WifiConnected=InitWIFI("Redmi 9T","87654321")
+    }
 })
